e218c7cf44f0e65a1c8fc74d9df4f25a16538864
子平六 <liuziping@gmail.com>
2023-11-05 17:39:37 -0600

    Update lightbox.cy.ts

--- cypress/e2e/lightbox.cy.ts
+++ cypress/e2e/lightbox.cy.ts
@@ -5,7 +5,7 @@ describe("Testing Lightbox PDF Rendering Configuration", () => {
     beforeEach(() => {
         cy.viewport(1920, 1080);
     });
-    it.skip("Should visit the deployed canary static page", () => {
+    it("Should visit the deployed canary static page", () => {
         cy.visit(Cypress.env("BASE_URL") + Cypress.env("DEFAULT_PATH"));
         cy.findByTestId('test-link').contains('Test').scrollIntoView({
             duration: 1000
@@ -15,56 +15,32 @@ describe("Testing Lightbox PDF Rendering Configuration", () => {
 
     it("Should visit the deployed canary, then navigate via the header to the lightbox page route", () => {
         cy.visit(Cypress.env("BASE_URL") + Cypress.env("DEFAULT_PATH"));
-        
         cy.findByTestId('test-link').click();
         cy.contains('Lightbox').click();
-
         cy.addEventListenerAdobeReady().then((ready: any) => {
-
-       
-       
             cy.spy(ready.AdobeDC, 'View').as('adobeDCSpy');
-          
-           
         } );
-
         //listen for network
         cy.intercept('POST', 'https://viewlicense.adobe.io/viewsdklicense/jwt').as('getLicense');
         cy.contains('Toggle Light Box').click();
 
         
-
-
         cy.wait('@getLicense',{
             timeout: 10000,
         }).then((_interception: any) => {
-
+        
             cy.get('@adobeDCSpy').should('be.calledOnce');
         });
-
         // go to previous page
-
         cy.go('back');
-    
         cy.location(
             "pathname"
         ).hash().should("eq", "#/test");
-
-      
-        cy.wait(1000);
-
-        
-
+        cy.wait(1000); // adding wait only the lengthen the test duration 
         cy.go('forward');
-
-
-
         cy.location(
             "pathname"
         ).hash().should("eq", "#/light");
-
         cy.contains('Toggle Light Box').should('be.visible');
-
-
     });
 });
